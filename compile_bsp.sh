971c545705db5dd331a25e36d8eefa7887469e21
csauve <csauve@t3hz0r.com>
2020-07-12 00:44:11 -0700

    Update blender export script and get current BSP ingame

--- compile_bsp.sh
+++ compile_bsp.sh
@@ -4,21 +4,19 @@ set -e
 #set these...
 #WINEPREFIX
 #HALO_HOME
-#JMS_EXPORTER
 
 RADIOSITY_QUALITY=0.8
 RADIOSITY_STOP=0.08
 
-blender \
-  -b data/levels/alpine/models/alpine.blend \
-  --python $JMS_EXPORTER \
+blender -b \
+  data/levels/alpine/models/alpine.blend \
+  --python blender-export.py \
   -- \
   --filepath data/levels/alpine/models/alpine \
-  --encoding utf_8 \
-  --jms_version 3 \
-  --game_version haloce \
   --extension ".JMS" \
-  --triangulate_faces false # this flag doesnt work, but its OK
+  --jms_version 8200 \
+  --game_version haloce \
+  --triangulate_faces
 
 # assuming shaders already exist
 wine "$HALO_HOME/tool.exe" structure levels\\alpine alpine
