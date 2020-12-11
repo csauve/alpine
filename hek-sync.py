e1e95b5d450d5974840c5e1b4367473d43b5b6ae
csauve <csauve@t3hz0r.com>
2020-12-11 01:41:18 -0800

    Retexture skybox, add billboard trees, more red base lighting

--- hek-sync.py
+++ hek-sync.py
@@ -17,10 +17,18 @@ def sync(pattern):
     for f in {*dev_files.keys(), *hek_files.keys()}:
         if f in dev_files and (f not in hek_files or dev_files[f] > hek_files[f]):
             print("Copying to HEK: " + f)
-            shutil.copy(path.join(dev_root, f), path.join(hek_root, f))
+            shutil.copy2(path.join(dev_root, f), path.join(hek_root, f))
         elif f in hek_files and (f not in dev_files or hek_files[f] > dev_files[f]):
             print("Copying to local: " + f)
-            shutil.copy(path.join(hek_root, f), path.join(dev_root, f))
+            shutil.copy2(path.join(hek_root, f), path.join(dev_root, f))
+
+sync("data/levels/alpine/**/**.tif")
+sync("data/levels/alpine/**/**.JMS")
+sync("data/levels/alpine/**/**.hsc")
+sync("data/scenery/wildflower/**/**.tif")
+sync("data/scenery/wildflower/**/**.JMS")
+sync("data/sky/sky_alpine/**/**.tif")
+sync("data/sky/sky_alpine/**/**.JMS")
 
 sync("tags/levels/alpine/**/**.*")
 sync("tags/scenery/wildflower/**/**.*")
